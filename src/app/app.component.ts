211c979aab045f481178567a2821c92b66170363
Luis Hernandez <luis.fdo.hdez@gmail.com>
2020-08-04 23:01:35 -0500

    ejemplo de drag and drop elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import sampleJson from '../assets/pantallas.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'drag-btns';
+  elements = [];
+  selectedElements = [];
+  isOver: boolean;
+
+  constructor(private http: HttpClient) {}
+
+
+  ngOnInit() {
+    this.elements = sampleJson.FFFFFFF01P02;
+    console.log(sampleJson.FFFFFFF01P02);
+  }
+
+  dropDiv(event) {
+    const data = JSON.parse(event.dataTransfer.getData('element'));
+    this.selectedElements.push(data);
+    this.isOver = false;
+    console.log(data);
+  }
+
+  onDrag(event, element) {
+    event.dataTransfer.setData('element', JSON.stringify(element));
+  }
+
+  allowDrop(event) {
+    event.preventDefault();
+    this.isOver = true;
+  }
 }
